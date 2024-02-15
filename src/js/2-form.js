3eff745c426bede0056b586029e1d15b6bfc0499
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-02-15 17:15:06 +0200

    clear_code

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,80 +1,38 @@
+
+
+const FORM_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
-const inputEmail = form.querySelector('input[name="email"]');
-const inputText = form.querySelector('textarea[name="message"]');
 
-const currentObj = {
-    email: '',
-    message: '',
-};
+form.addEventListener('input', onInputData);
+form.addEventListener('submit', onFormSubmit);
 
-const localStorageKey = "feedback-form-state";
-let localStorageData = '';
+let dataForm = JSON.parse(localStorage.getItem(FORM_KEY)) || {};
+const { email, message } = form.elements;
+reloadPage();
 
-
-function saveToLS(key = 'empty', value = ' '){
-    const jsonData = JSON.stringify(value);
-    localStorage.setItem(key, jsonData);
+function onInputData(event) {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(FORM_KEY, JSON.stringify(dataForm));
 }
 
-function loadFromLS(key){
-    const data = localStorage.getItem(key);
+function reloadPage() {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+  }
+}
 
-    try {
-        const result = JSON.parse(data);
-        return result
-    }  catch {
-        return data;
-    }
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log({ email: email.value, message: message.value });
 
+  if (email.value === '' || message.value === '') {
+    return alert(`Заповніть всі обов'язкові поля.`);
+  }
+
+  localStorage.removeItem(FORM_KEY);
+  event.currentTarget.reset();
+  dataForm = {};
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-// try {
-//     localStorageData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
-    
-//     if (localStorageData && typeof localStorageData === 'object') {
-//         currentObj.email = localStorageData.email || '';
-//         currentObj.message = localStorageData.message || '';
-//         inputEmail.value = currentObj.email;
-//         inputText.value = currentObj.message;
-//     }
-// } catch (error) {
-//     console.error('Error parsing JSON:', error);
-// }
-
-// const upLoadNewDate = (event) => {
-//     const target = event.target;
-//     if (target === inputEmail) {
-//         currentObj.email = target.value.trim();
-//     } else if (target === inputText) {
-//         currentObj.message = target.value.trim();
-//     }
-//     localStorageData = JSON.stringify(currentObj);
-//     localStorage.setItem(localStorageKey, localStorageData);
-// };
-
-// form.addEventListener('input', upLoadNewDate);
-
-// form.addEventListener('submit', (event) => {
-//     event.preventDefault();
-//     if (inputEmail.value.trim() === "" || inputText.value.trim() === '') {
-//         alert(`Please fill in all form fields before submitting.`);
-//         return;
-//     }
-//     console.log(JSON.parse(localStorage.getItem(localStorageKey)));
-//     localStorage.removeItem(localStorageKey);
-//     currentObj.email = '';
-//     currentObj.message = '';
-//     form.reset();
-// });
