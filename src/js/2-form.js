15f21ca84481277446e7e63a0b3422aa53b6b2aa
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-04-11 23:53:48 +0300

    fix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -11,12 +11,12 @@ const { email, message } = form.elements;
 
 
 function onInputData(event) {
-  dataForm = { email: email.value, message: message.value };
+  dataForm[event.target.name] = event.target.value.trim();
   localStorage.setItem(FORM_KEY, JSON.stringify(dataForm));
 }
 
 function reloadPage() {
-  if (dataForm) {
+  if (dataForm.email || dataForm.message) {
     email.value = dataForm.email || '';
     message.value = dataForm.message || '';
   }
@@ -26,16 +26,14 @@ reloadPage();
 
 function onFormSubmit(event) {
   event.preventDefault();
-  console.log({ email: email.value, message: message.value });
 
-  if (email.value === '' || message.value === '') {
+  if (!email.value.trim() || !message.value.trim()) {
     return alert(`Заповніть всі обов'язкові поля.`);
   }
+
+  console.log({ email: email.value.trim(), message: message.value.trim() });
 
   localStorage.removeItem(FORM_KEY);
   event.currentTarget.reset();
   dataForm = {};
 }
-
-
-// варіант 2 як на уроці  
